0b19f8b1cb67c02c4a9f4f76a638d811c087766f
Iman <iman1991@mail.ru>
2017-12-04 13:11:39 +0300

    zabbix not blocked

--- agent.py
+++ agent.py
@@ -25,41 +25,40 @@ def add_data(host, key, clock, value):
 
 
 def start_agent(row):
-    while True:
-        clock = int(time.time())
-        request = {
+    clock = int(time.time())
+    request = {
             "request": "agent data",
             "data": [],
             "clock": clock
         }
 
-        for key in row:
-            try:
-                if key == "ping":
-                    request["data"].append(add_data(hostname, key, clock, 1))
-                elif key == "leftFromPaid":
-                    request["data"].append(add_data(hostname, key, clock, row[key] // 100))
-                elif key == "sessionPaid":
-                    request["data"].append(add_data(hostname, key, clock, row[key] // 100))
-                elif key == "totalPaid":
-                    request["data"].append(add_data(hostname, key, clock, row[key] // 100))
-                else:
-                    request["data"].append(add_data(hostname, key, clock, row[key]))
-
-            except:
-                pass
+    for key in row:
         try:
-            raw = zbx.get_data_to_send(json.dumps(request))
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.connect((server, port))
-            sock.send(raw)
-            data = sock.recv(5)
-            if data == zbx.ZABBIX_HEADER:
-                data = sock.recv(8)
-                len_pack = struct.unpack("i", data[:4])[0]
-                sock.recv(len_pack)
-                sock.close()
-        except Exception as e:
-            print(e)
-        time.sleep(10)
+            if key == "ping":
+                request["data"].append(add_data(hostname, key, clock, 1))
+            elif key == "leftFromPaid":
+                request["data"].append(add_data(hostname, key, clock, row[key] // 100))
+            elif key == "sessionPaid":
+                request["data"].append(add_data(hostname, key, clock, row[key] // 100))
+            elif key == "totalPaid":
+                request["data"].append(add_data(hostname, key, clock, row[key] // 100))
+            else:
+                request["data"].append(add_data(hostname, key, clock, row[key]))
+
+        except:
+                pass
+    try:
+        raw = zbx.get_data_to_send(json.dumps(request))
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((server, port))
+        sock.send(raw)
+        data = sock.recv(5)
+        if data == zbx.ZABBIX_HEADER:
+            data = sock.recv(8)
+            len_pack = struct.unpack("i", data[:4])[0]
+            sock.recv(len_pack)
+            sock.close()
+    except Exception as e:
+        print(e)
+    time.sleep(10)
 
